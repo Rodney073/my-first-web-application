7f7e0a84dba6791ccc01f177f46cbc6e9f8dc89a
Rodney073 <63658050+Rodney073@users.noreply.github.com>
2020-04-30 21:39:39 +0200

    layout ready

--- strings.js
+++ strings.js
@@ -1,1 +1,3 @@
 $('.photoframe').append('<img id="bigpic" src="https://i.imgur.com/GNNcXpQ.jpg" alt="">');
+$('.textbox').append('<h1>So Cute!</h1>');
+$('.textbox').append('<p>Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>');
